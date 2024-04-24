459ecd891591dbd1254dee5819a528cf7063a38e
Amartya Varma <amartya.varma@gmail.com>
2024-04-24 16:53:57 +0100

    f

--- msinference.py
+++ msinference.py
@@ -130,7 +130,7 @@ def get_device_names():
         device_count = torch.cuda.device_count()
         for i in range(device_count):
             device = torch.cuda.get_device_name(i)
-            device_names.append(device)
+            device_names.append(f"cuda:{i}")
     else:
         device_names.append("cpu")
     return device_names
