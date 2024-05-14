852c5dedc421aa24538a5d81a57e435184f05db2
Amartya Varma <amartya.varma@gmail.com>
2024-05-14 13:53:17 +0100

    f

--- api.py
+++ api.py
@@ -115,7 +115,7 @@ async def process_speaker(request: SpeakerRequest):
     audio = audio / 32768.0
     audio = audio.reshape(1, -1)
     audio = torch.tensor(audio)
-    write('voices/user.wav', 24000, voice)
+    write('voices/user.wav', 24000)
     voice = msinference.compute_style("voices/user.wav")
     
     voices["user"] = voice
