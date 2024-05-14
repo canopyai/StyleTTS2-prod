36a7f8a5d5071310a7a04fd21c4bf6cc14089ee2
Amartya Varma <amartya.varma@gmail.com>
2024-05-14 14:55:45 +0100

    f

--- api.py
+++ api.py
@@ -64,7 +64,6 @@ def synthesize(text, steps = 10, alpha_ = 0.1, beta_ = 0.1, voice_vector = [1,0,
         style = style + style_scaled if style is not None else style_scaled
 
     if(voice_vector[2] == 1):
-        style = voices["user"][device_index]
         model_name = "base"
 
     return msinference.inference(
